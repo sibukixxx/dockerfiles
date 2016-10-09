cabe1a4991444c8bdcc70438d557a17e4f194e7a
sibukixxx <sibukixxx@gmail.com>
2016-10-10 01:41:54 +0900

    add python

--- python3_centos7/run.sh
+++ python3_centos7/run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 ## 作成したPython入りのCentOSイメージからコンテナ作成
-docker run -it --name dev-python3 sibukixxx/python3 /bin/bash
+docker run -it -v `pwd`/src:/tmp --name dev-python3 sibukixxx/python3 /bin/bash
